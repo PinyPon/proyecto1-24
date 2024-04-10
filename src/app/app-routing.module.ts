3d33feffaea1ede3844a9dbc2a421f6c96777f5e
PinyPon <tizianaperfumobouzon@gmai.com>
2024-04-10 10:56:04 -0300

    menu con vistas vinculadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { GaleriaComponent } from './componentes/galeria/galeria.component';
+import { ServiciosComponent } from './componentes/servicios/servicios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  path: "inicio", component: InicioComponent
+  },
+  {
+    path: "galeria", component: GaleriaComponent
+  },
+  {
+    path: "servicios", component: ServiciosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+ }
