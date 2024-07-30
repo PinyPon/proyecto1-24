b8fade79221e761164e652babe18ce36c2b73dfb
PinyPon <tizianaperfumobouzon@gmail.com>
2024-07-30 10:56:58 -0300

    Creamos interfaz productos y empezamos con el crud. Definimos estructura de table

---
+++ src/app/models/producto.ts
@@ -0,0 +1,9 @@
+export interface Producto {
+    idProducto: string;
+    nombre: string;
+    precio: number;
+    descripcion: string;
+    categoria: string;
+    imagen: url;
+    alt:  string;
+}
--- src/app/modules/admin/components/table/table.component.ts
+++ src/app/modules/admin/components/table/table.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Producto } from 'src/app/models/producto';
+import { CrudService } from '../../services/crud.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-table',
@@ -6,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./table.component.css']
 })
 export class TableComponent {
+coleccionProductos: Producto[] = [];
+//definimos formularios para los productos
+producto = new FormGroup({
+  nombre: new FormControl('', Validators.required),
+  precio: new FormControl(0,Validators.required),
+  descripcion: new FormControl('',Validators.required),
+  categoria: new FormControl('',Validators.required),
+  imagen:new FormControl('',Validators.required),
+  alt:new FormControl('',Validators.required)
+})
 
+constructor(public servicioCrud: CrudService){}
 }
